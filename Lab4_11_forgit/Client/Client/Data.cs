b5559d0915a79a56becf3d1b2117060b55c19fa9
Satsko <68208375+Satsko@users.noreply.github.com>
2023-02-28 14:20:37 +0300

    Add files via upload

---
+++ Lab4_11_forgit/Client/Client/Data.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class ImagesContext : DbContext
+{
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Result> Results { get; set; }
+    public ImagesContext() => Database.EnsureCreated();
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.UseSqlite("Data Source=ImgDB1.db");
+    }
+}
+
+public class Image
+{
+    [Key]
+    public int ImageId { get; set; }
+    public string name { get; set; }
+    public string path { get; set; }
+    public byte[] hash { get; set; }
+    public byte[] bytes { get; set; }
+    public Image() { }
+    public Image(string nameimg, string pathimg, byte[] hashimg, byte[] byteimg)
+    {
+        name = nameimg;
+        path = pathimg;
+        hash = hashimg;
+        bytes = byteimg;
+    }
+}
+
+public class Result
+{
+    [Key]
+    [ForeignKey(nameof(Image))]
+    public int ImageId { get; set; }
+    public string name { get; set; }
+
+    public List<(string, double)> emotions;
+    public Result() { emotions = new List<(string, double)>(); }
+    public Result(int imageId, string nameimg, List<(string, double)> emotionsimg)
+    {
+        //ImageId = imageId;
+        name = nameimg;
+        emotions = new List<(string, double)>();
+        emotions = emotionsimg;
+    }
+}
